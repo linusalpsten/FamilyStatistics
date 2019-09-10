1b7f1745c29fbe09142da66fadf65a0fb30db254
Linus Alpsten <linus.alpsten@telia.com>
2019-09-10 09:29:30 +0200

    add RelationType enum

---
+++ FamilyStatistics/FamilyStatistics/RelationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyStatistics
+{
+    enum RelationType
+    {
+        Parent, Sibling, Cousin, Grandmother, Grandfather, Aunt, Uncle
+    }
+}
