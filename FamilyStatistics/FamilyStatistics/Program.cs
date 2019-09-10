1318ba53753a81226c9d63a8f32843803760e738
Linus Alpsten <linus.alpsten@telia.com>
2019-09-10 11:53:15 +0200

    add variables

--- FamilyStatistics/FamilyStatistics/Program.cs
+++ FamilyStatistics/FamilyStatistics/Program.cs
@@ -8,6 +8,15 @@ namespace FamilyStatistics
 {
     class Program
     {
+        static string[] names = new string[128];
+        static int[] ages = new int[128];
+        static double[] heights = new double[128];
+        static RelationType[] relations = new RelationType[8];
+
+        const string defaultName = null;
+        const int defautAge = 0;
+        const double defaultHeight = 0;
+        const RelationType defaultRelation = RelationType.Default;
         static void Main(string[] args)
         {
 
